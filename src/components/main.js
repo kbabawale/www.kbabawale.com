58f7e4de03424bb3a54c305159bb63c03a7a3564
Kolapo Babawale <kolapobabawale@gmail.com>
2019-10-24 16:09:49 +0100

    Added new routes

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,13 @@ import Homepage from './homepage';
 import NotFound from './notfound';
 import Blog from './blog';
 import BlogsPage from './blogspage';
+import Login from './login';
+import Users from './users';
+import Adduser from './add_user';
+import Edituser from './edit_user';
+import Addarticle from './add_article';
+import Articles from './articles';
+import EditArticle from './edit_article';
 
 
 export default class Main extends React.Component {
@@ -14,6 +21,13 @@ export default class Main extends React.Component {
                     <Route exact path="/" component={Homepage} />
                     <Route exact path="/blogs" component={BlogsPage} />
                     <Route exact path="/blog/:id" component={Blog} />
+                    <Route exact path="/articles" component={Articles} />
+                    <Route exact path="/article/add" component={Addarticle} />
+                    <Route exact path="/users" component={Users} />
+                    <Route exact path="/users/add" component={Adduser} />
+                    <Route exact path="/user/edit/:id" component={Edituser} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/article/edit/:id" component={EditArticle} />
                     <Route path="/reload" component={null} key="reload" />
                     <Route component={NotFound} />
                 </Switch>
