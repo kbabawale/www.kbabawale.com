b9e2b660556150cd2409133756f3ec95b80b0052
Kolapo Babawale <kolapobabawale@gmail.com>
2019-10-25 11:31:09 +0100

    tablet and mobile resp navbar toggle

--- src/components/header2.js
+++ src/components/header2.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../img/logo-main.png';
 import { NavLink } from 'react-router-dom';
 // import "../App.css";
 
 function Header2() {
+    const [nav, setNav] = useState(true);
+
+    const toggleMobileMenu = () => {
+
+        nav === true ? setNav(false) : setNav(true);
+    }
+
+    useEffect(() => {
+        //get device width and decide whether to show navbar or not
+        var width = (window.innerWidth > 0) ? window.innerWidth : 0;
+        if (width < 1025) {
+            setNav(false);
+        }
+    }, []);
+
     return (
         <div className="container-fluid">
             <header className="container">
@@ -11,20 +26,23 @@ function Header2() {
                     <div className="col-6 pull-left"> <NavLink exact to="/"><img src={Logo} className="img-responsive" /></NavLink></div>
                     <div className="col-6 text-right pt-2">
                         <span className="welcome white_normal">Welcome, Kolapo</span>&nbsp;&nbsp;&nbsp;
-                        <button type="button" className=""><i className="fa fa-bars"></i></button>
-                        <div className="menu-nav">
-                            <ul>
-                                <li>
-                                    <NavLink className="subtitle" exact to="/users">Users</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink className="subtitle" exact to="/blogs">Articles</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink className="subtitle" exact to="/">Logout</NavLink>
-                                </li>
-                            </ul>
-                        </div>
+                        <button type="button" onClick={() => { toggleMobileMenu(); }} className="hamburger"><i className="fa fa-bars"></i></button>
+                        {nav ? (
+                            <div className="menu-nav">
+                                <ul>
+                                    <li>
+                                        <NavLink className="subtitle" exact to="/users">Users</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink className="subtitle" exact to="/blogs">Articles</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink className="subtitle" exact to="/">Logout</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        ) : ''}
+
                     </div>
                 </div>
             </header>
